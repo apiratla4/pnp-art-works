5806296a15ae9d744e849878baf5cbc1cf02c4dd
dhanunjay20 <dhanunjay@hiresoftsolutions.com>
2025-10-28 16:07:33 +0530

    get all order details based on order id backend controller updatedd

--- server/src/routes/orders.routes.js
+++ server/src/routes/orders.routes.js
@@ -8,7 +8,7 @@ router.post('/', orders.createOrder);
 router.get('/', orders.getOrders);
 router.get('/:id', orders.getOrder);
 router.patch('/:id', orders.updateOrderStatus);
-router.get('/orders/ref/:referenceId', getOrderByReferenceId);
+router.get('/orders/ref/:referenceId', orders.getOrderByReferenceId);
 router.patch('/paypal/:paypalOrderId/capture', orders.updateOrderWithCapture);
 router.post('/paypal/:paypalOrderId/webhook', orders.addWebhookEvent);
 
